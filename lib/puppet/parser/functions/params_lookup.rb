10986ce93d0fac2cc666f38a0bdbb10a7863ba38
Jenkins @ Example42 <roots@lab42.it>
2012-02-08 00:02:43 +0100

    Fixed default return value for params_lookup

--- lib/puppet/parser/functions/params_lookup.rb
+++ lib/puppet/parser/functions/params_lookup.rb
@@ -4,45 +4,46 @@
 # This function lookups for a variable value in various locations
 # following this order
 # - Hiera backend, if present
-# - ::varname (if option abs_lookup is true)
+# - ::varname (if second argument is 'global')
 # - ::modulename_varname
 # - ::modulename::params::varname
-# - The value set with the "default" option
 #
 # It's based on a suggestion of Dan Bode on how to better manage
 # Example42 NextGen modules params lookups.
 # Code has been written with the decisive help of Brice Figureau,
 # Peter Meier and Ohad Levy during the Fosdem 2012 days (thanks guys)
 #
+# Tested ad adapted to Puppet 2.6.x and later
+#
 # Alessandro Franceschi al@lab42.it
 # 
 module Puppet::Parser::Functions
   newfunction(:params_lookup, :type => :rvalue, :doc => <<-EOS
 This fuction looks for the given variable name in a set of different sources:
-- Hiera, if available ('varname' if option abs_lookup is true)
+- Hiera, if available (if second argument is 'global')
 - Hiera, if available ('modulename_varname')
-- ::varname (if option abs_lookup is true)
+- ::varname (if second argument is 'global')
 - ::modulename_varname
 - ::modulename::params::varname
-- The value set with the "default" option
+If no value is found in the defined sources, it returns an empty string ('')
     EOS
   ) do |arguments|
 
     raise(Puppet::ParseError, "params_lookup(): Define at least the variable name " +
       "given (#{arguments.size} for 1)") if arguments.size < 1
 
-    value = :undef
+    value = ''
     var_name = arguments[0]
     # options = { 'default' => :undef, 'abs_lookup' => false }.merge(arguments[1] || {}) 
     module_name = parent_module_name
     if Puppet::Parser::Functions.function('hiera')
-      value = function_hiera("#{var_name}",:undef) if arguments[1] == 'global'
-      value = function_hiera("#{module_name}_#{var_name}",:undef) 
+      value = function_hiera("#{var_name}",'') if arguments[1] == 'global'
+      value = function_hiera("#{module_name}_#{var_name}",'') 
     end
-    if value == :undef
+    if value == ''
       value = lookupvar("::#{var_name}") if arguments[1] == 'global'
       value = lookupvar("::#{module_name}_#{var_name}")
-      value = lookupvar("::#{module_name}::params::#{var_name}") if value == :undef
+      value = lookupvar("::#{module_name}::params::#{var_name}") if value == ''
     end
     
     return value
