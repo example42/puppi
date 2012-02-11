5d2bbd5ea3db9fea6e016d4325c6b24f448cafd0
Alessandro Franceschi <al@lab42.it>
2012-02-11 21:55:51 +0100

    Fixed params_lookup overrides

--- lib/puppet/parser/functions/params_lookup.rb
+++ lib/puppet/parser/functions/params_lookup.rb
@@ -10,9 +10,9 @@
 #
 # It's based on a suggestion of Dan Bode on how to better manage
 # Example42 NextGen modules params lookups.
-# Code has been written with the decisive help of Brice Figureau,
-# Peter Meier and Ohad Levy during the Fosdem 2012 days (thanks guys)
-#
+# Major help has been given by  Brice Figureau, Peter Meier
+# and Ohad Levy during the Fosdem 2012 days (thanks guys)
+# 
 # Tested ad adapted to Puppet 2.6.x and later
 #
 # Alessandro Franceschi al@lab42.it
@@ -34,18 +34,19 @@ If no value is found in the defined sources, it returns an empty string ('')
 
     value = ''
     var_name = arguments[0]
-    # options = { 'default' => :undef, 'abs_lookup' => false }.merge(arguments[1] || {}) 
     module_name = parent_module_name
+    
     if Puppet::Parser::Functions.function('hiera')
       value = function_hiera("#{var_name}",'') if arguments[1] == 'global'
       value = function_hiera("#{module_name}_#{var_name}",'') 
     end
+
     if value == ''
       value = lookupvar("::#{var_name}") if arguments[1] == 'global'
-      value = lookupvar("::#{module_name}_#{var_name}")
+      value = lookupvar("::#{module_name}_#{var_name}") if ( lookupvar("::#{module_name}_#{var_name}") != :undefined && lookupvar("::#{module_name}_#{var_name}") != '' )
       value = lookupvar("::#{module_name}::params::#{var_name}") if (value == :undefined || value == '') 
     end
-    
+
     return value
   end
 end
