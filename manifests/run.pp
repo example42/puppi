24703dbc3afe0e73ccb9c439794f163b72473eb9
Alessandro Franceschi <al@lab42.it>
2012-09-15 23:53:57 +0200

    Revert "fixes Issue #30 dependency issue prevents auto_deploy"
    This reverts commit 8be2786a492e2a8dd146f1ef55dab94fd460700c.

--- manifests/run.pp
+++ manifests/run.pp
@@ -25,6 +25,6 @@ define puppi::run (
     creates => "${puppi::params::archivedir}/puppirun_${name}",
   }
 
-  Class['puppi::is_installed'] -> Puppi::Run[$name]
+  Puppi::Run[$name] -> Class['puppi::is_installed']
 
 }
