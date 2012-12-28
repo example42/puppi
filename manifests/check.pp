accfa77254ef25b6ac0c1f69e1fb95bdbbf56e57
Alessandro Franceschi <al@lab42.it>
2012-12-28 22:34:35 +0100

    Added base_dir parameter to define puppi::check commands base path

--- manifests/check.pp
+++ manifests/check.pp
@@ -14,6 +14,7 @@
 #
 define puppi::check (
   $command,
+  $base_dir = '',
   $hostwide = 'no',
   $priority = '50',
   $project  = 'default',
@@ -24,6 +25,11 @@ define puppi::check (
 
   $ensure = bool2ensure($enable)
   $bool_hostwide = any2bool($hostwide)
+
+  $real_base_dir = $base_dir ? {
+    ''      => $puppi::params::checkpluginsdir,
+    default => $base_dir,
+  }
 
   $path = $bool_hostwide ? {
     true  => "${puppi::params::checksdir}/${priority}-${name}" ,
@@ -37,7 +43,7 @@ define puppi::check (
     owner   => $puppi::params::configfile_owner,
     group   => $puppi::params::configfile_group,
     require => Class['puppi'],
-    content => "${puppi::params::checkpluginsdir}/${command}\n",
+    content => "${real_base_dir}/${command}\n",
     tag     => 'puppi_check',
   }
 
