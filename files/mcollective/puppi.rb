91f691f38d9ce721d9a64b033522a7709b0e3662
Kim Neunert <k9ert@gmx.de>
2013-04-09 22:32:26 +0200

    improved mcoll-agent especially the deploy-action

--- files/mcollective/puppi.rb
+++ files/mcollective/puppi.rb
@@ -13,42 +13,59 @@ module MCollective
 #                   validate :project, :shellsafe
                     project = request[:project] if request[:project]
                     reply.data = %x[puppi check #{project}].chomp
-#                    reply.exitcode = $?.exitstatus
+                    if ($?.exitstatus.int > 0)
+                      reply.fail "FAILED: #{reply.data}"
+                    end
             end
 
             def info_action
 #                   validate :project, :shellsafe
                     project = request[:project] if request[:project]
                     reply.data = %x[puppi info #{project}].chomp
-#                    reply.exitcode = $?.exitstatus
+                    if ($?.exitstatus.int > 0)
+                      reply.fail "FAILED: #{reply.data}"
+                    end
             end
 
             def log_action
 #                   validate :project, :shellsafe
                     project = request[:project] if request[:project]
                     reply.data = %x[puppi log #{project} -c 10].chomp
-#                    reply.exitcode = $?.exitstatus
+                    if ($?.exitstatus.int > 0)
+                      reply.fail "FAILED: #{reply.data}"
+                    end
             end
 
             def deploy_action
                     validate :project, :shellsafe
                     project = request[:project] if request[:project]
-                    reply.data = %x[puppi deploy #{project}].chomp
-#                    reply.exitcode = $?.exitstatus
+                    if (!File.directory? "/etc/puppi/projects/#{project}")
+                      reply.fail "No such project #{project}"
+                      return
+                    end
+                    puppioptions = request[:puppioptions]
+                    reply.data += %x[puppi deploy #{project} -o "#{puppioptions}"].chomp
+                    if ($?.exitstatus.int > 0)
+                      reply.fail "FAILED: #{reply.data}"
+                    end
             end
 
             def rollback_action
                     validate :project, :shellsafe
                     project = request[:project] if request[:project]
                     reply.data = %x[puppi rollback #{project} latest].chomp
-#                    reply.exitcode = $?.exitstatus
+                    if ($?.exitstatus.int > 0)
+                      reply.fail "FAILED: #{reply.data}"
+                    end
             end
 
             def init_action
                     validate :project, :shellsafe
                     project = request[:project] if request[:project]
                     reply.data = %x[puppi init #{project}].chomp
-#                    reply.exitcode = $?.exitstatus
+                    if ($?.exitstatus.int > 0)
+                      reply.fail "FAILED: #{reply.data}"
+                    end
             end
 
         end
