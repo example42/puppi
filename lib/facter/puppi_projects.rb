94d53263201883227c990c06e77e39bdda19c5b7
tuxmaster5000 <837503+tuxmaster5000@users.noreply.github.com>
2023-05-02 15:02:35 +0200

    Fix for Puppet 8 (#161)

--- lib/facter/puppi_projects.rb
+++ lib/facter/puppi_projects.rb
@@ -2,6 +2,6 @@ require 'facter'
 Facter.add("puppi_projects") do
   confine :kernel => [ 'Linux' , 'SunOS' , 'FreeBSD' , 'Darwin' ]
   setcode do
-    Facter::Util::Resolution.exec('ls `grep projectsdir  /etc/puppi/puppi.conf | sed \'s/projectsdir="\([^"]*\)"/\1/\'` | tr \'\n\' \',\' | sed \'s/,$//\'') if File.exists?("/etc/puppi/puppi.conf")
+    Facter::Util::Resolution.exec('ls `grep projectsdir  /etc/puppi/puppi.conf | sed \'s/projectsdir="\([^"]*\)"/\1/\'` | tr \'\n\' \',\' | sed \'s/,$//\'') if File.exist?("/etc/puppi/puppi.conf")
   end
 end
