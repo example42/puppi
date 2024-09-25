932e7f2235544bd4773165472dd523d2b12b44bd
Alessandro Franceschi <al@example42.com>
2024-09-25 18:26:12 +0200

    Last?

--- spec/defines/project_spec.rb
+++ spec/defines/project_spec.rb
@@ -6,19 +6,18 @@ describe 'puppi::project' do
   on_supported_os.select { |k, _v| k == 'redhat-8-x86_64'  }.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-      let(:title) { 'puppi::project' }
+      let(:title) { 'get' }
       let(:node) { 'rspec.example42.com' }
       let(:pre_condition) { 'include puppi' }
       let(:params) {
         {
           'enable'   =>  'true',
-          'name'     =>  'get',
         }
       }
     
       describe 'Test puppi project configuration file' do
         it 'should create a puppi::project configuration file' do
-          should contain_file('/etc/puppi/projects/myapp/config').with_ensure('present')
+          should contain_file('/etc/puppi/projects/get/config').with_ensure('present')
         end
       end
 
